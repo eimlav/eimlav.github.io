0954025e4fb85080a3fb93cf0edc630cfc2846f8
eimlav <eimlav123@gmail.com>
2017-12-18 20:11:14 +0000

    Now displays results from Wikipedia

--- js/wikisearch.js
+++ js/wikisearch.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
   $("#search").on("keyup", function (e) {
-      console.log($("#search").val());
-
-      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Albert%20Einstein&utf8=&format=json", function(val) {
-        console.log(val);
-      });
+      //console.log($("#search").val());
+      sendSearchRequest();
   });
 
-  function httpGetAsync(theUrl, callback)
+  function sendSearchRequest()
   {
-      var xmlHttp = new XMLHttpRequest();
-
-      xmlHttp.onreadystatechange = function() {
-          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-              callback(xmlHttp.responseText);
+    var query = $("#search").val().replace(/ /g, "&20");
+    console.log(query);
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + query + "&utf8=&format=json&callback=\?", function(val) {
+      console.log(val);
+      $(".main").html("");
+      for (var i = 0; i < 10; i++) {
+        console.log("Search Result " + i + val.query.search[i].title);
+        var link = "https://en.wikipedia.org/wiki/" + val.query.search[i].title.replace(/ /g, "_");
+        $(".main").append("<hr><a target=\"_blank\" href=\"" + link + "\"><div class=\"result\"><h3 class=\"result-title\">" + val.query.search[i].title + "</h3><p class=\"result-text\">" + val.query.search[i].snippet + "...</p></div></a>");
       }
-      xmlHttp.open("GET", theUrl, true);
-      xmlHttp.setRequestHeader("Access-Control-Allow-Origin", theUrl);
-      xmlHttp.send();
+    });
   }
 });
